2f23e2495711e8f8e658f990c1e389698af44b2c
Dorottya <macidori98@gmail.com>
2021-07-23 20:28:24 +0200

    game working

--- screens/QuestionsScreen.js
+++ screens/QuestionsScreen.js
@@ -1,115 +1,98 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet, ActivityIndicator, FlatList, TouchableOpacity, Image } from 'react-native';
 import Error from '../components/Error';
-import { getRegionDetails } from '../services/service';
-import { generateGuessTheNeighbourQuestions, getAllQuestions } from '../utils/RandomQuestionSelector';
+import { generateGuessCapitalQuestions, generateGuessFlagQuestions, generateGuessTheNeighbourQuestions } from '../utils/RandomQuestionSelector';
 import Colors from '../theme/Colors';
 import moment from 'moment';
-import { getDurationString, shuffle } from '../utils/Helpers';
+import { getDurationString } from '../utils/Helpers';
 
-var correctAnswers = 0;
+var correctAnswer;
 
 const QuestionsScreen = ({ navigation, route }) => {
     const data = route.params.data;
     const [loading, setLoading] = React.useState(true);
     const [questionIndex, setQuestionIndex] = React.useState();
-    const [gameQuestions, setGameQuestions] = React.useState([]);
-    const [otherAnswers, setOtherAnswers] = React.useState([]);
     const [error, setError] = React.useState();
     const [startDateAndTime, setStartDateAndTime] = React.useState(Date.now());
     const [gameData, setGameData] = React.useState();
 
-    const onItemSelected = (item, isNeighborMode = false) => {
-        if (isNeighborMode) {
-            if (gameQuestions[questionIndex].borders.indexOf(item.alpha3Code) > -1) {//it contains
-                correctAnswers++;
-            }
-        } else {
-            if (item.capital == gameQuestions[questionIndex].capital) {
-                correctAnswers++;
-            }
-        }
-
-        if (questionIndex + 1 == data.questionNo) {
-            var endDate = Date.now();
-            var durationInMillis = endDate - startDateAndTime;
-
-            var duration = getDurationString(durationInMillis);
-
-            navigation.navigate(route.params.nextRoute, {
-                data: {
-                    correctAns: correctAnswers,
-                    startDate: moment(startDateAndTime).format('DD MMM yyyy, HH:mm'),
-                    duration: duration,
-                },
-            });
-
-            return;
-        }
-
-        setQuestionIndex(questionIndex + 1);
-    };
-
-    const onItemSelectedNeighbor = (item) => {
+    const onItemSelected = (item) => {
         if (item == gameData[questionIndex].correctAnswer) {
-            correctAnswers++;
+            correctAnswer++;
         }
 
         if (questionIndex + 1 == data.questionNo) {
-            var endDate = Date.now();
-            var durationInMillis = endDate - startDateAndTime;
-
-            var duration = getDurationString(durationInMillis);
-
-            navigation.navigate(route.params.nextRoute, {
-                data: {
-                    correctAns: correctAnswers,
-                    startDate: moment(startDateAndTime).format('DD MMM yyyy, HH:mm'),
-                    duration: duration,
-                },
-            });
-
+            endGame();
             return;
         }
 
         setQuestionIndex(questionIndex + 1);
     };
 
-    const getAllAnswers = () => {
-        var a = [...otherAnswers[questionIndex]];
-        shuffle(a);
-        return a;
+    const endGame = () => {
+        var endDate = Date.now();
+        var durationInMillis = endDate - startDateAndTime;
+
+        var duration = getDurationString(durationInMillis);
+
+        navigation.navigate(route.params.nextRoute, {
+            data: {
+                correctAns: correctAnswer,
+                startDate: moment(startDateAndTime).format('DD MMM yyyy, HH:mm'),
+                duration: duration,
+            },
+        });
     };
 
     React.useEffect(() => {
-        if (data.type === 'Guess the neighbor') {
-            generateGuessTheNeighbourQuestions(data.region, data.questionNo)
-                .then((resp) => {
-                    setGameData(resp);
-                    setQuestionIndex(0);
-                    setLoading(false);
-                    setStartDateAndTime(Date.now());
-                })
-                .catch((err) => {
-                    console.log(err);
-                    setError(err);
-                    setLoading(false);
-                });
-        } else {
-            getRegionDetails(data.region)
-                .then(resp => {
-                    const array = getAllQuestions(resp, data.questionNo);
-                    setGameQuestions(array.correctAnswers);
+        correctAnswer = 0;
 
-                    setOtherAnswers(array.allQuestions);
-                    setQuestionIndex(0);
-                    setLoading(false);
-                    setStartDateAndTime(Date.now());
-                })
-                .catch(err => {
-                    setError(err);
-                    setLoading(false);
-                });
+        switch (data.type) {
+            case 'Guess the neighbor':
+                generateGuessTheNeighbourQuestions(data.region, data.questionNo)
+                    .then((resp) => {
+                        setGameData(resp);
+                        setQuestionIndex(0);
+                        setLoading(false);
+                        setStartDateAndTime(Date.now());
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        setError(err);
+                        setLoading(false);
+                    });
+                break;
+            case 'Guess the flag':
+                generateGuessFlagQuestions(data.region, data.questionNo)
+                    .then((resp) => {
+                        setGameData(resp);
+                        setQuestionIndex(0);
+                        setLoading(false);
+                        setStartDateAndTime(Date.now());
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        setError(err);
+                        setLoading(false);
+                    });
+                break;
+            case 'Guess the capital':
+                generateGuessCapitalQuestions(data.region, data.questionNo)
+                    .then((resp) => {
+                        setGameData(resp);
+                        setQuestionIndex(0);
+                        setLoading(false);
+                        setStartDateAndTime(Date.now());
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        setError(err);
+                        setLoading(false);
+                    });
+                break;
+
+            default:
+                break;
         }
     }, [data]);
 
@@ -119,24 +102,22 @@ const QuestionsScreen = ({ navigation, route }) => {
             {!loading && !error && data.type === 'Guess the capital' && (
                 <View>
                     <View>
-                        <Text style={styles.question}>{data.type} of {gameQuestions[questionIndex].name}</Text>
+                        <Text style={styles.question}>{data.type} of {gameData[questionIndex].country}</Text>
                     </View>
                     <View style={styles.list}>
                         <FlatList
-                            data={getAllAnswers()}
+                            data={gameData[questionIndex].options}
                             keyExtractor={(item, index) => index}
                             renderItem={({ item }) =>
                                 <TouchableOpacity
                                     style={styles.listItem}
                                     onPress={() => onItemSelected(item)}>
                                     <Text style={styles.centeredText}>
-                                        {item.capital}
+                                        {item}
                                     </Text>
                                 </TouchableOpacity>} />
                     </View>
-
                 </View>
-
             )}
             {!loading && !error && data.type === 'Guess the flag' && (
                 <View>
@@ -146,17 +127,17 @@ const QuestionsScreen = ({ navigation, route }) => {
                     <Image
                         resizeMode={'cover'}
                         style={styles.image}
-                        source={{ uri: `https://www.countryflags.io/${gameQuestions[questionIndex].alpha2Code}/shiny/64.png` }} />
+                        source={{ uri: `https://www.countryflags.io/${gameData[questionIndex].country}/shiny/64.png` }} />
                     <View style={styles.list}>
                         <FlatList
-                            data={getAllAnswers()}
+                            data={gameData[questionIndex].options}
                             keyExtractor={(item, index) => index}
                             renderItem={({ item }) =>
                                 <TouchableOpacity
                                     style={styles.listItem}
                                     onPress={() => onItemSelected(item)}>
                                     <Text style={styles.centeredText}>
-                                        {item.name}
+                                        {item}
                                     </Text>
                                 </TouchableOpacity>} />
                     </View>
@@ -176,7 +157,7 @@ const QuestionsScreen = ({ navigation, route }) => {
                             renderItem={({ item }) =>
                                 <TouchableOpacity
                                     style={styles.listItem}
-                                    onPress={() => onItemSelectedNeighbor(item)}>
+                                    onPress={() => onItemSelected(item)}>
                                     <Text style={styles.centeredText}>
                                         {item}
                                     </Text>
