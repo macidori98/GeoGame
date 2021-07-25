b2898cab3f1246685ef7023f45b6291552e290a6
Dorottya <macidori98@gmail.com>
2021-07-25 17:36:30 +0200

    added error and loading elements

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -1,13 +1,16 @@
-import { View, SafeAreaView, RefreshControl, FlatList, StyleSheet } from 'react-native';
+import { View, SafeAreaView, RefreshControl, FlatList, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import React from 'react';
 import { readData } from '../services/SaveData';
 import { ScrollView } from 'react-native-gesture-handler';
 import ResultsCard from '../components/ResultsCard';
+import Error from '../components/Error';
+import Colors from '../theme/Colors';
 
 const StatisticsScreen = ({ navigation, route }) => {
     const [refreshing, setRefreshing] = React.useState(false);
     const [gameData, setGameData] = React.useState([]);
     const [error, setError] = React.useState();
+    const [loading, setLoading] = React.useState(true);
     const nextRoute = route.params.nextRoute;
 
     const onRefresh = React.useCallback(() => {
@@ -20,9 +23,11 @@ const StatisticsScreen = ({ navigation, route }) => {
             .then((result) => {
                 setGameData(result.reverse());
                 setRefreshing(false);
+                setLoading(false);
             })
             .catch(err => {
                 setError(err);
+                setLoading(false);
             });
     };
 
@@ -39,14 +44,20 @@ const StatisticsScreen = ({ navigation, route }) => {
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <ScrollView
-                    refreshControl={<RefreshControl refreshing={refreshing}
-                        onRefresh={onRefresh} />}>
-                    <FlatList data={gameData}
-                        keyExtractor={(item, index) => index}
-                        renderItem={({ item }) =>
-                            <ResultsCard onItemClick={onItemClick} item={item} />} />
-                </ScrollView>
+                {loading && !error && (<ActivityIndicator style={styles.center} size="large" color={Colors.primary} />)}
+                {gameData.length === 0 && !error && !loading && (
+                    <Text>No played games data</Text>
+                )}
+                {gameData.length > 0 && !error && !loading && (
+                    <ScrollView
+                        refreshControl={<RefreshControl refreshing={refreshing}
+                            onRefresh={onRefresh} />}>
+                        <FlatList data={gameData}
+                            keyExtractor={(item, index) => index}
+                            renderItem={({ item }) =>
+                                <ResultsCard onItemClick={onItemClick} item={item} />} />
+                    </ScrollView>)}
+                {error && !loading && <Error />}
             </View>
         </SafeAreaView>
     );
